6a54a90cb0e60da74725848c5403ba4867fcf2cd
Nvcoding <166248158+Nvcoing@users.noreply.github.com>
2025-07-24 17:03:09 +0700

    Update main.py

--- llm_server/main.py
+++ llm_server/main.py
@@ -1,6 +1,7 @@
 # main.py
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from load_model import load_model_tokenizer
 from generate import generate_stream
@@ -8,6 +9,13 @@ from generate import generate_stream
 MODEL_PATH = "NV9523/MentalGPT"  # Hoặc đường dẫn local nếu đã tải model
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cho phép mọi domain gọi (bạn có thể thay bằng domain cụ thể)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 model, tokenizer, device = load_model_tokenizer(MODEL_PATH)
 
 class PromptRequest(BaseModel):
